dfac498ae0b168fba8f69d06570ab6d7e4b6dfb5
Gieberly <129142725+Gieberly@users.noreply.github.com>
2024-05-07 14:45:12 +0800

    Update cover.php

--- cover.php
+++ cover.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
-<body>
+<body>  
     
     <div class="overlay_student"></div>
  
