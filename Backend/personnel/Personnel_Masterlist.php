2965d4dab65b5de163e89a3faa054024d7a0c67e
Gieberly <129142725+Gieberly@users.noreply.github.com>
2024-05-09 06:39:37 +0800

    Update Personnel_Masterlist.php

--- Backend/personnel/Personnel_Masterlist.php
+++ Backend/personnel/Personnel_Masterlist.php
@@ -108,6 +108,7 @@ $stmt->close();
 
 <head>
 
+
     <!-- jQuery and DataTables JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -129,6 +130,8 @@ $stmt->close();
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js"></script>
+
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 </head>
 
@@ -2331,6 +2334,7 @@ table.dataTable tbody tr:hover,
             }
         }
     </style>
+
 
 
     </div>
