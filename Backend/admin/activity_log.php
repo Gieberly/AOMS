a0ea373fdb45c4cc54af072a7b563830dfc4cd4e
Gieberly <129142725+Gieberly@users.noreply.github.com>
2024-05-16 10:38:38 +0800

    Update activity_log.php

--- Backend/admin/activity_log.php
+++ Backend/admin/activity_log.php
@@ -686,9 +686,9 @@ $result = $conn->query($query);
         <main>
             <div class="head-title">
                 <div class="left">
-                <h1>Audit Trail</h1>
+                    <h1>Applicants</h1>
                     <ul class="breadcrumb">
-                        <li><a href="#">Audit Trail</a></li>
+                        <li><a href="#">Applicants</a></li>
                         <li><i class='bx bx-chevron-right'></i></li>
                         <li>
                         <li><a class="active" href="Admin_Dashboard.php">Home</a></li>
